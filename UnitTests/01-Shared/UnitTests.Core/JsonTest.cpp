18bcf339fa093216e2d91d0e11105acbfc51c148
waYne <wayneison@gmx.at>
2019-11-07 13:51:55 +0100

    worked a bit on JsonWriterTests

--- UnitTests/01-Shared/UnitTests.Core/JsonTest.cpp
+++ UnitTests/01-Shared/UnitTests.Core/JsonTest.cpp
@@ -29,7 +29,7 @@ namespace UnitTestsCore
 	TEST_CLASS(Core_Json)
 	{
 	public:
-		TEST_METHOD(WriterFixed)
+		TEST_METHOD(WriterFixedObject)
 		{
 			// prepare
 			MemoryStream Target = MemoryStream();
@@ -38,55 +38,82 @@ namespace UnitTestsCore
 
 			// write
 			JsonWriter.WriteStartObject();
+			JsonWriter.WritePropertyName(L"Yep");
+			JsonWriter.WriteValue(true);
+			JsonWriter.WritePropertyName(L"Nope");
+			JsonWriter.WriteValue(false);
+			JsonWriter.WritePropertyName(L"NullValue");
+			JsonWriter.WriteNull();
 			JsonWriter.WritePropertyName(L"Int");
 			JsonWriter.WriteValue(5448);
-			JsonWriter.WritePropertyName(L"Bool1");
-			JsonWriter.WriteValue(true);
-			JsonWriter.WritePropertyName(L"Bool2");
-			JsonWriter.WriteValue(false);
 			JsonWriter.WritePropertyName(L"Float");
 			JsonWriter.WriteValue(13.37f);
 			JsonWriter.WritePropertyName(L"Double");
 			JsonWriter.WriteValue(13.37);
 			JsonWriter.WritePropertyName(L"String");
 			JsonWriter.WriteValue(L"SomeValue");
-
-			// ToDo: this results in invalid json
-			JsonWriter.WritePropertyName(L"IntArray");
-			JsonWriter.WriteStartArray();
-			JsonWriter.WriteValue(1);
-			JsonWriter.WriteValue(2);
-			JsonWriter.WriteValue(3);
-			JsonWriter.WriteEndArray();
-			
+			JsonWriter.WritePropertyName(L"EmptyObject");
+			JsonWriter.WriteStartObject();
+			JsonWriter.WriteEndObject();
 			JsonWriter.WritePropertyName(L"SomeObject");
 			JsonWriter.WriteStartObject();
 			JsonWriter.WritePropertyName(L"Property1");
 			JsonWriter.WriteValue(L"Value1");
 			JsonWriter.WritePropertyName(L"Property2");
 			JsonWriter.WriteValue(L"Value2");
-			JsonWriter.WritePropertyName(L"Property3");
-			JsonWriter.WriteStartArray();
-			/*
-			// ToDo: this results in invalid json
-			JsonWriter.WriteStartArray();
-			JsonWriter.WriteEndArray();
-			JsonWriter.WriteStartArray();
-			JsonWriter.WriteEndArray();
-			*/
-			JsonWriter.WriteEndArray();
 			JsonWriter.WriteEndObject();
-			/*
-			// ToDo: this results in invalid json - can there even be an empty object or should it be null?
-			JsonWriter.WritePropertyName(L"SomeEmptyObject");
+			JsonWriter.WritePropertyName(L"EmptyArray");
+			JsonWriter.WriteStartArray();
+			JsonWriter.WriteEndArray();
+			JsonWriter.WritePropertyName(L"IntArray");
+			JsonWriter.WriteStartArray();
+			JsonWriter.WriteValue(1);
+			JsonWriter.WriteValue(2);
+			JsonWriter.WriteValue(3);
+			JsonWriter.WriteEndArray();
+			JsonWriter.WritePropertyName(L"ObjectArray");
+			JsonWriter.WriteStartArray();
 			JsonWriter.WriteStartObject();
-			JsonWriter.WriteEndObject();
-			*/
-			JsonWriter.WritePropertyName(L"NullValue");
-			JsonWriter.WriteNull();
-			JsonWriter.WritePropertyName(L"String");
+			JsonWriter.WritePropertyName(L"Value1");
+			JsonWriter.WriteValue(5448);
+			JsonWriter.WritePropertyName(L"Value2");
 			JsonWriter.WriteValue(L"SomeValue");
 			JsonWriter.WriteEndObject();
+			JsonWriter.WriteStartObject();
+			JsonWriter.WritePropertyName(L"Value1");
+			JsonWriter.WriteValue(5448);
+			JsonWriter.WritePropertyName(L"Value2");
+			JsonWriter.WriteValue(L"SomeValue");
+			JsonWriter.WriteEndObject();
+			JsonWriter.WriteEndArray();
+			JsonWriter.WritePropertyName(L"TwoDimensionalArray");
+			JsonWriter.WriteStartArray();
+			JsonWriter.WriteStartArray();
+			JsonWriter.WriteEndArray();
+			JsonWriter.WriteStartArray();
+			JsonWriter.WriteEndArray();
+			JsonWriter.WriteEndArray();
+			JsonWriter.WritePropertyName(L"SomeOtherString");
+			JsonWriter.WriteValue(L"SomeValue");
+			JsonWriter.WriteEndObject();
+			
+			// check
+		}
+		TEST_METHOD(WriterFixedArray)
+		{
+			// prepare
+			MemoryStream Target = MemoryStream();
+			StreamWriter Writer = StreamWriter(Target);
+			JsonTextWriter JsonWriter = JsonTextWriter(Writer);
+
+			// write
+			JsonWriter.WriteStartArray();
+			/*
+			JsonWriter.WriteValue(1);
+			JsonWriter.WriteValue(2);
+			JsonWriter.WriteValue(3);
+			*/
+			JsonWriter.WriteEndArray();
 
 			// check
 		}
