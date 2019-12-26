890e84fdcf7cb3ef9b8be2f1f485c6d694f14d1a
waYne <wayneison@gmx.at>
2019-12-27 00:00:10 +0100

    made sure to check whether the calculation has actually run on another thread

--- UnitTests/02-Windows/UnitTests.Core/ThreadTest.cpp
+++ UnitTests/02-Windows/UnitTests.Core/ThreadTest.cpp
@@ -32,24 +32,29 @@ namespace UnitTestsCore
 			Thread T = Thread();
 			T.Start(Delegate<void>::CreateDelegate<Core_Threading_Thread, &Core_Threading_Thread::ZeroParameterThreadStart>(this));
 			Assert::AreEqual(25, _CalculatedValue);
+			Assert::IsFalse(std::this_thread::get_id() == _WorkerThreadId);
 		}
 		TEST_METHOD(ParameterizedThreadStartThread)
 		{
 			Thread T = Thread();
 			T.Start<int>(Delegate<void, int>::CreateDelegate<Core_Threading_Thread, &Core_Threading_Thread::SingleParameterThreadStart>(this), 6);
 			Assert::AreEqual(36, _CalculatedValue);
+			Assert::IsFalse(std::this_thread::get_id() == _WorkerThreadId);
 		}
 	private:
 		int _OriginalValue = 5;
 		int _CalculatedValue;
+		std::thread::id _WorkerThreadId;
 
 		void ZeroParameterThreadStart()
 		{
 			_CalculatedValue = _OriginalValue * _OriginalValue;
+			_WorkerThreadId = std::this_thread::get_id();
 		}
 		void SingleParameterThreadStart(int OriginalValue)
 		{
 			_CalculatedValue = OriginalValue * OriginalValue;
+			_WorkerThreadId = std::this_thread::get_id();
 		}
 	};
 }
