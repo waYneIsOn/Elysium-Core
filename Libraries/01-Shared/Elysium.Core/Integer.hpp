e5f2bc017362ed997a30d45501a28a67283eeab4
waYne <wayneison@gmx.at>
2020-01-16 13:29:13 +0100

    ...

--- Libraries/01-Shared/Elysium.Core/Integer.hpp
+++ Libraries/01-Shared/Elysium.Core/Integer.hpp
@@ -19,10 +19,10 @@ namespace Elysium::Core
 	typedef unsigned short uint16_t;
 	typedef unsigned int uint32_t;
 	typedef unsigned long long uint64_t;
-
+	/*
 	constexpr const int16_t INT16_MIN = (-32767i16 - 1);
-	constexpr const int16_t INT32_MIN = (-2147483647i32 - 1);
-	constexpr const int16_t INT64_MIN = (-9223372036854775807i64 - 1);
+	constexpr const int32_t INT32_MIN = (-2147483647i32 - 1);
+	constexpr const int64_t INT64_MIN = (-9223372036854775807i64 - 1);
 
 	constexpr const int16_t INT16_MAX = 32767i16;
 	constexpr const int32_t INT32_MAX = 2147483647i32;
@@ -31,5 +31,6 @@ namespace Elysium::Core
 	constexpr const uint16_t UINT16_MAX = 0xffffui16;
 	constexpr const uint32_t UINT32_MAX = 0xffffffffui32;
 	constexpr const uint64_t UINT64_MAX = 0xffffffffffffffffui64;
+	*/
 }
 #endif
