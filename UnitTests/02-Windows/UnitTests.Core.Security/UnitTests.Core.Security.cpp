702ef58ce8a0b6c96943b32b862cc84b90e8a451
waYne <wayneison@gmx.at>
2020-07-10 08:44:51 +0200

    added security unittest project

---
+++ UnitTests/02-Windows/UnitTests.Core.Security/UnitTests.Core.Security.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestsCoreSecurity
+{
+	TEST_CLASS(UnitTestsCoreSecurity)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
