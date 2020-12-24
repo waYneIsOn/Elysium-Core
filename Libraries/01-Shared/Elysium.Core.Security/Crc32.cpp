fd6f1a8567eb49d5a039f35362aa4c830831d416
waYne <wayneison@gmx.at>
2020-12-24 19:57:38 +0100

    added crc32-class

---
+++ Libraries/01-Shared/Elysium.Core.Security/Crc32.cpp
@@ -0,0 +1,29 @@
+#include "Crc32.hpp"
+
+#ifndef ELYSIUM_CORE_ARGUMENTNULLEXCEPTION
+#include "../Elysium.Core/ArgumentNullException.hpp"
+#endif
+
+constexpr const Elysium::Core::uint32_t Elysium::Core::Security::Cryptography::Crc32::CalculateBytewise(const Elysium::Core::byte* Data, const size_t Length)
+{
+	if (Data == nullptr)
+	{
+		throw ArgumentNullException(u8"Data");
+	}
+
+	if (Length == 0)
+	{
+		return 0;
+	}
+
+	// Todd K. Moon, "Error Correction Coding. Mathematical Methods and Algorithms", Wiley, 2005, ISBN 0-471-64800-0
+	Elysium::Core::uint32_t Checksum = 0xFFFFFFFF;
+	Elysium::Core::uint32_t LookupTableIndex;
+	for (size_t i = 0; i < Length; i++)
+	{
+		LookupTableIndex = (Data[i] ^ Checksum) & 0xFF;
+		Checksum = (Checksum >> 8) ^ _LookupTable[LookupTableIndex];
+	}
+
+	return ~Checksum;
+}
