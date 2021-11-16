f65ef1a6e1aad4ebb1714d64f9b63767980ac69c
waYne <cm.pillarbox@gmail.com>
2021-11-16 02:44:56 +0100

    small string optimization

--- Libraries/01-Shared/Elysium.Core.Template/String.hpp
+++ Libraries/01-Shared/Elysium.Core.Template/String.hpp
@@ -12,12 +12,24 @@ Copyright (c) waYne (CAM). All rights reserved.
 #pragma once
 #endif
 
+#ifndef ELYSIUM_CORE_PRIMITIVES
+#include "Primitives.hpp"
+#endif
+
+#ifndef ELYSIUM_CORE_SYSTEM
+#include "System.hpp"
+#endif
+
 #ifndef ELYSIUM_CORE_TEMPLATE_CONCEPTS_CHARACTER
 #include "Character.hpp"
 #endif
 
 #ifndef ELYSIUM_CORE_TEMPLATE_MEMORY_DEFAULTALLOCATOR
 #include "DefaultAllocator.hpp"
+#endif
+
+#ifndef ELYSIUM_CORE_TEMPLATE_TEXT_STRINGTRAITS
+#include "StringTraits.hpp"
 #endif
 
 namespace Elysium::Core::Template::Text
@@ -27,39 +39,187 @@ namespace Elysium::Core::Template::Text
 	{
 	public:
 		using ConstCharacter = const C;
-		using Pointer = C*;
-		using ConstPointer = const C*;
+		using CharPointer = C*;
+		using ConstCharPointer = const C*;
+	private:
+		struct HeapString final
+		{
+			CharPointer _Data; // 4/8 bytes
+			Elysium::Core::size _Size; // 4/8 bytes
+			Elysium::Core::size _CapacityAndFlag; // 4/8 bytes -> 1 bit flag (stack/heap allocated) + 31/63 bit capacity
+
+			Elysium::Core::size GetCapacity() const noexcept;
+
+			void SetCapacity(const Elysium::Core::size Value) noexcept;
+		};	// 12/24 bytes
+
+		struct StackString final
+		{
+			Elysium::Core::byte _Data[sizeof(HeapString) - sizeof(Elysium::Core::byte)]; // 11/23 bytes
+			Elysium::Core::byte _RemainingBytesAndFlag;	// 1 byte -> 1 bit flag (stack/heap allocated) + 7 bit remaining size
+
+			Elysium::Core::size GetSize() const noexcept;
+
+			void SetSize(const Elysium::Core::size Value) noexcept;
+		};	// 12/24 bytes
 	public:
 		StringBase() noexcept;
-		StringBase(ConstPointer Value) noexcept;
-		StringBase(const StringBase& Source) = delete;
-		StringBase(StringBase&& Right) noexcept = delete;
-		~StringBase();
 
+		StringBase(ConstCharPointer Value) noexcept;
+
+		StringBase(ConstCharPointer Value, const Elysium::Core::size Length) noexcept;
+
+		StringBase(const StringBase& Source) = delete;
+
+		StringBase(StringBase&& Right) noexcept = delete;
+
+		~StringBase();
+	public:
 		StringBase<C, Allocator>& operator=(const StringBase& Source) = delete;
+
 		StringBase<C, Allocator>& operator=(StringBase&& Right) noexcept = delete;
-		StringBase<C, Allocator>& operator=(ConstPointer Value) = delete;
+
+		StringBase<C, Allocator>& operator=(ConstCharPointer Value) = delete;
+	public:
+		//static const bool IsEmpty(const StringBase& Value);
+	public:
+		const Elysium::Core::size GetSize() const;
+
+		const Elysium::Core::size GetCapacity() const;
 	private:
 		inline static Allocator _Allocator = Allocator();
+		inline static constexpr const Elysium::Core::byte _MaximumByteSizeOnStack = sizeof(HeapString) - 1;
+		inline static constexpr const size_t _HeapStackFlagShift = (sizeof(Elysium::Core::size) - 1) * 8;
+#ifdef ELYSIUM_CORE_LITTLEENDIAN
+		inline static constexpr const Elysium::Core::size _HeapStackFlagMask = 0x01;
+		inline static constexpr const Elysium::Core::size _CapacityExtractMask = ~(Elysium::Core::size(_HeapStackFlagMask) << _HeapStackFlagShift);
+#else
+		// ...
+#endif
 	private:
-		/*
-		Elysium::Core::size _Capacity;
-		Elysium::Core::size _Size;
-		Pointer _Data;
-		*/
+		union
+		{
+			HeapString _Heap;
+			StackString _Stack;
+		} _InternalString;
+	private:
+		void CopyHeapString(ConstCharPointer Value, const Elysium::Core::size ByteLength);
+
+		void CopyStackString(ConstCharPointer Value, const Elysium::Core::size ByteLength);
+
+		const bool IsHeapAllocated() const;
 	};
 
 	template<Concepts::Character C, class Allocator>
+	inline Elysium::Core::size StringBase<C, Allocator>::HeapString::GetCapacity() const noexcept
+	{
+#ifdef ELYSIUM_CORE_LITTLEENDIAN
+		return _CapacityAndFlag & _CapacityExtractMask;
+#else
+		throw 1;
+#endif
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline void StringBase<C, Allocator>::HeapString::SetCapacity(const Elysium::Core::size Value) noexcept
+	{
+#ifdef ELYSIUM_CORE_LITTLEENDIAN
+		// most right bit of last byte (0000 0000 - ... - 0000 000x) is stack/heap-flag
+		_CapacityAndFlag = 0x01_ui64 << _HeapStackFlagShift |	// make sure stack-flag is set to one
+			Value;
+#else
+		throw 1;
+#endif
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline Elysium::Core::size StringBase<C, Allocator>::StackString::GetSize() const noexcept
+	{	// most right bit (0000 000x) is stack/heap-flag, all other bits represent the remaining bytes
+		return sizeof(HeapString) - 1 - (_RemainingBytesAndFlag >> 1);
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline void StringBase<C, Allocator>::StackString::SetSize(const Elysium::Core::size Value) noexcept
+	{	// seven most left bits represent the remaining bytes, most right bit represents stack-bitflag (0000 000F)
+		_RemainingBytesAndFlag = 0x00 |	// make sure stack-flag is set to zero
+			static_cast<Elysium::Core::byte>(((sizeof(HeapString) - 1 - Value) << 1));
+	}
+
+	template<Concepts::Character C, class Allocator>
 	inline StringBase<C, Allocator>::StringBase() noexcept
+		: StringBase<C, Allocator>(nullptr, 0)
 	{ }
 
 	template<Concepts::Character C, class Allocator>
-	inline StringBase<C, Allocator>::StringBase(ConstPointer Value) noexcept
+	inline StringBase<C, Allocator>::StringBase(ConstCharPointer Value) noexcept
+		: StringBase<C, Allocator>(Value, StringTraits<C>::GetLength(Value))
 	{ }
+
+	template<Concepts::Character C, class Allocator>
+	inline StringBase<C, Allocator>::StringBase(ConstCharPointer Value, const Elysium::Core::size Length) noexcept
+		: _InternalString{ 0x00 }
+	{
+		const Elysium::Core::size ValueByteLength = Length * sizeof(C);
+		if (ValueByteLength > _MaximumByteSizeOnStack)
+		{
+			CopyHeapString(Value, ValueByteLength);
+		}
+		else
+		{
+			CopyStackString(Value, ValueByteLength);
+		}
+	}
 
 	template<Concepts::Character C, class Allocator>
 	inline StringBase<C, Allocator>::~StringBase()
-	{ }
+	{
+		if (IsHeapAllocated())
+		{
+			if (_InternalString._Heap._Data != nullptr)
+			{
+				_Allocator.Deallocate(_InternalString._Heap._Data, 0);
+				_InternalString._Heap._Data = nullptr;
+			}
+		}
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline const Elysium::Core::size StringBase<C, Allocator>::GetSize() const
+	{
+		return IsHeapAllocated() ? _InternalString._Heap._Size : _InternalString._Stack.GetSize() / sizeof(C);
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline const Elysium::Core::size StringBase<C, Allocator>::GetCapacity() const
+	{
+		return IsHeapAllocated() ? _InternalString._Heap.GetCapacity() : _MaximumByteSizeOnStack / sizeof(C);
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline void StringBase<C, Allocator>::CopyHeapString(ConstCharPointer Value, const Elysium::Core::size ByteLength)
+	{
+		const Elysium::Core::size ByteLengthIncludingNullTerminationCharacter = ByteLength + 1;
+
+		_InternalString._Heap._Data = _Allocator.Allocate(ByteLengthIncludingNullTerminationCharacter);
+		memcpy(&_InternalString._Heap._Data[0], Value, ByteLengthIncludingNullTerminationCharacter * sizeof(C));
+
+		const Elysium::Core::size Length = ByteLength / sizeof(C);
+		_InternalString._Heap._Size = Length;
+		_InternalString._Heap.SetCapacity(Length);
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline void StringBase<C, Allocator>::CopyStackString(ConstCharPointer Value, const Elysium::Core::size ByteLength)
+	{
+		memcpy(&_InternalString._Stack._Data[0], Value, ByteLength);
+		_InternalString._Stack.SetSize(ByteLength);
+	}
+
+	template<Concepts::Character C, class Allocator>
+	inline const bool StringBase<C, Allocator>::IsHeapAllocated() const
+	{	// most right bit (0000 000x)
+		return (_InternalString._Stack._RemainingBytesAndFlag & _HeapStackFlagMask) == _HeapStackFlagMask;
+	}
 
 	using String = StringBase<char>;
 	using WideString = StringBase<wchar_t>;
--- UnitTests/01-Shared/UnitTests.Core.Template/StringTests.cpp
+++ UnitTests/01-Shared/UnitTests.Core.Template/StringTests.cpp
@@ -13,13 +13,115 @@ namespace UnitTests::Core::Template::Text
 	TEST_CLASS(StringTests)
 	{
 	public:
-		TEST_METHOD(Bla)
+		TEST_METHOD(SizesAndCapacities)
 		{
-			Elysium::Core::Template::Text::String CharArray = "";
-			Elysium::Core::Template::Text::WideString WCharArray = L"";
-			Elysium::Core::Template::Text::Utf8String Utf8CharArray = u8"";
-			Elysium::Core::Template::Text::Utf16String Utf16CharArray = u"";
-			Elysium::Core::Template::Text::Utf32String Utf32CharArray = U"";
+			// sizes
+			constexpr const Elysium::Core::size StringSize = sizeof(Elysium::Core::Template::Text::String);
+			constexpr const Elysium::Core::size WideStringSize = sizeof(Elysium::Core::Template::Text::WideString);
+			constexpr const Elysium::Core::size Utf8StringSize = sizeof(Elysium::Core::Template::Text::Utf8String);
+			constexpr const Elysium::Core::size Utf16StringSize = sizeof(Elysium::Core::Template::Text::Utf16String);
+			constexpr const Elysium::Core::size Utf32StringSize = sizeof(Elysium::Core::Template::Text::Utf32String);
+
+			Assert::AreEqual(24_ui64, StringSize);
+			Assert::AreEqual(24_ui64, WideStringSize);
+			Assert::AreEqual(24_ui64, Utf8StringSize);
+			Assert::AreEqual(24_ui64, Utf16StringSize);
+			Assert::AreEqual(24_ui64, Utf32StringSize);
+
+			// empty strings
+			Elysium::Core::Template::Text::String EmptyCharArray = Elysium::Core::Template::Text::String();
+			Elysium::Core::Template::Text::WideString EmptyWideCharArray = Elysium::Core::Template::Text::WideString();
+			Elysium::Core::Template::Text::Utf8String EmptyUtf8CharArray = Elysium::Core::Template::Text::Utf8String();
+			Elysium::Core::Template::Text::Utf16String EmptyUtf16CharArray = Elysium::Core::Template::Text::Utf16String();
+			Elysium::Core::Template::Text::Utf32String EmptyUtf32CharArray = Elysium::Core::Template::Text::Utf32String();
+
+			Assert::AreEqual(0_ui64, EmptyCharArray.GetSize());
+			Assert::AreEqual(0_ui64, EmptyWideCharArray.GetSize());
+			Assert::AreEqual(0_ui64, EmptyUtf8CharArray.GetSize());
+			Assert::AreEqual(0_ui64, EmptyUtf16CharArray.GetSize());
+			Assert::AreEqual(0_ui64, EmptyUtf32CharArray.GetSize());
+
+			Assert::AreEqual(23_ui64, EmptyCharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, EmptyWideCharArray.GetCapacity());
+			Assert::AreEqual(23_ui64, EmptyUtf8CharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, EmptyUtf16CharArray.GetCapacity());
+			Assert::AreEqual(5_ui64, EmptyUtf32CharArray.GetCapacity());
+
+			// null strings
+			Elysium::Core::Template::Text::String NullCharArray = Elysium::Core::Template::Text::String(nullptr);
+			Elysium::Core::Template::Text::WideString NullWideCharArray = Elysium::Core::Template::Text::WideString(nullptr);
+			Elysium::Core::Template::Text::Utf8String NullUtf8CharArray = Elysium::Core::Template::Text::Utf8String(nullptr);
+			Elysium::Core::Template::Text::Utf16String NullUtf16CharArray = Elysium::Core::Template::Text::Utf16String(nullptr);
+			Elysium::Core::Template::Text::Utf32String NullUtf32CharArray = Elysium::Core::Template::Text::Utf32String(nullptr);
+
+			Assert::AreEqual(0_ui64, NullCharArray.GetSize());
+			Assert::AreEqual(0_ui64, NullWideCharArray.GetSize());
+			Assert::AreEqual(0_ui64, NullUtf8CharArray.GetSize());
+			Assert::AreEqual(0_ui64, NullUtf16CharArray.GetSize());
+			Assert::AreEqual(0_ui64, NullUtf32CharArray.GetSize());
+
+			Assert::AreEqual(23_ui64, NullCharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, NullWideCharArray.GetCapacity());
+			Assert::AreEqual(23_ui64, NullUtf8CharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, NullUtf16CharArray.GetCapacity());
+			Assert::AreEqual(5_ui64, NullUtf32CharArray.GetCapacity());
+			
+			// stack strings with maximum size
+			Elysium::Core::Template::Text::String MaxStackCharArray = Elysium::Core::Template::Text::String("12345678911131517192123");
+			Elysium::Core::Template::Text::WideString MaxStackWideCharArray = Elysium::Core::Template::Text::WideString(L"12345678911");
+			Elysium::Core::Template::Text::Utf8String MaxStackUtf8CharArray = Elysium::Core::Template::Text::Utf8String(u8"12345678911131517192123");
+			Elysium::Core::Template::Text::Utf16String MaxStackUtf16CharArray = Elysium::Core::Template::Text::Utf16String(u"12345678911");
+			Elysium::Core::Template::Text::Utf32String MaxStackUtf32CharArray = Elysium::Core::Template::Text::Utf32String(U"12345");
+
+			Assert::AreEqual(23_ui64, MaxStackCharArray.GetSize());
+			Assert::AreEqual(11_ui64, MaxStackWideCharArray.GetSize());
+			Assert::AreEqual(23_ui64, MaxStackUtf8CharArray.GetSize());
+			Assert::AreEqual(11_ui64, MaxStackUtf16CharArray.GetSize());
+			Assert::AreEqual(5_ui64, MaxStackUtf32CharArray.GetSize());
+
+			Assert::AreEqual(23_ui64, MaxStackCharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, MaxStackWideCharArray.GetCapacity());
+			Assert::AreEqual(23_ui64, MaxStackUtf8CharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, MaxStackUtf16CharArray.GetCapacity());
+			Assert::AreEqual(5_ui64, MaxStackUtf32CharArray.GetCapacity());
+
+			// stack strings
+			Elysium::Core::Template::Text::String StackCharArray = Elysium::Core::Template::Text::String("xyz");
+			Elysium::Core::Template::Text::WideString StackWCharArray = Elysium::Core::Template::Text::WideString(L"xyz");
+			Elysium::Core::Template::Text::Utf8String StackUtf8CharArray = Elysium::Core::Template::Text::Utf8String(u8"xyz");
+			Elysium::Core::Template::Text::Utf16String StackUtf16CharArray = Elysium::Core::Template::Text::Utf16String(u"xyz");
+			Elysium::Core::Template::Text::Utf32String StackUtf32CharArray = Elysium::Core::Template::Text::Utf32String(U"xyz");
+
+			Assert::AreEqual(3_ui64, StackCharArray.GetSize());
+			Assert::AreEqual(3_ui64, StackWCharArray.GetSize());
+			Assert::AreEqual(3_ui64, StackUtf8CharArray.GetSize());
+			Assert::AreEqual(3_ui64, StackUtf16CharArray.GetSize());
+			Assert::AreEqual(3_ui64, StackUtf32CharArray.GetSize());
+
+			Assert::AreEqual(23_ui64, StackCharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, StackWCharArray.GetCapacity());
+			Assert::AreEqual(23_ui64, StackUtf8CharArray.GetCapacity());
+			Assert::AreEqual(11_ui64, StackUtf16CharArray.GetCapacity());
+			Assert::AreEqual(5_ui64, StackUtf32CharArray.GetCapacity());
+			
+			// heap strings
+			Elysium::Core::Template::Text::String HeapCharArray = Elysium::Core::Template::Text::String("this string is too large to fit onto stack");
+			Elysium::Core::Template::Text::WideString HeapWideCharArray = Elysium::Core::Template::Text::WideString(L"this string is too large to fit onto stack");
+			Elysium::Core::Template::Text::Utf8String HeapUtf8CharArray = Elysium::Core::Template::Text::Utf8String(u8"this string is too large to fit onto stack");
+			Elysium::Core::Template::Text::Utf16String HeapUtf16CharArray = Elysium::Core::Template::Text::Utf16String(u"this string is too large to fit onto stack");
+			Elysium::Core::Template::Text::Utf32String HeapUtf32CharArray = Elysium::Core::Template::Text::Utf32String(U"this string is too large to fit onto stack");
+
+			Assert::AreEqual(42_ui64, HeapCharArray.GetSize());
+			Assert::AreEqual(42_ui64, HeapWideCharArray.GetSize());
+			Assert::AreEqual(42_ui64, HeapUtf8CharArray.GetSize());
+			Assert::AreEqual(42_ui64, HeapUtf16CharArray.GetSize());
+			Assert::AreEqual(42_ui64, HeapUtf32CharArray.GetSize());
+			
+			Assert::AreEqual(42_ui64, HeapCharArray.GetCapacity());
+			Assert::AreEqual(42_ui64, HeapWideCharArray.GetCapacity());
+			Assert::AreEqual(42_ui64, HeapUtf8CharArray.GetCapacity());
+			Assert::AreEqual(42_ui64, HeapUtf16CharArray.GetCapacity());
+			Assert::AreEqual(42_ui64, HeapUtf32CharArray.GetCapacity());
 		}
 	};
 }
