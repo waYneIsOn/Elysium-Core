7ebf8066e6deabb6c444deaaa05f64b4a5f798b7
waYne <cm.pillarbox@gmail.com>
2024-03-18 11:36:16 +0100

    nothing

--- Libraries/01-Shared/Elysium.Core.Net/ProtocolType.hpp
+++ Libraries/01-Shared/Elysium.Core.Net/ProtocolType.hpp
@@ -28,29 +28,124 @@ namespace Elysium::Core::Net::Sockets
 	{
 		//Unknown = -1,
 
+		/// <summary>
+		/// 
+		/// </summary>
 		Unspecified = 0,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IP = 0,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6HopByHopOptions = 0,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Icmp = 1,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Igmp = 2,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Ggp = 3,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv4 = 4,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Tcp = 6,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Udp = 17,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Pup = 12,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Idp = 22,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6 = 41,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6RoutingHeader = 43,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6FragmentHeader = 44,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPSecEncapsulatingSecurityPayload = 50,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPSecAuthenticationHeader = 51,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IcmpV6 = 58,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6NoNextHeader = 59,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		IPv6DestinationOptions = 60,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		ND = 77,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Raw = 255,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Ipx = 1000,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		Spx = 1256,
+
+		/// <summary>
+		/// 
+		/// </summary>
 		SpxII = 1257
 	};
 }
