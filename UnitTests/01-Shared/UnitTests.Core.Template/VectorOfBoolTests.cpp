4e407d0d0240ea9d07e1c6613164ce74da7fd74a
waYne <cm.pillarbox@gmail.com>
2025-01-22 14:27:40 +0100

    prepared tests for other vectors and specializations

---
+++ UnitTests/01-Shared/UnitTests.Core.Template/VectorOfBoolTests.cpp
@@ -0,0 +1,23 @@
+#include "CppUnitTest.h"
+#include "../UnitTestExtensions/CppUnitTestFrameworkExtension.hpp"
+
+#include "../../../../Elysium-Core/Libraries/01-Shared/Elysium.Core/IndexOutOfRangeException.hpp"
+#include "../../../../Elysium-Core/Libraries/01-Shared/Elysium.Core/Primitives.hpp"
+#include "../../../../Elysium-Core/Libraries/01-Shared/Elysium.Core.Template/Move.hpp"
+#include "../../../../Elysium-Core/Libraries/01-Shared/Elysium.Core.Template/Vector.hpp"
+
+using namespace Elysium::Core;
+using namespace Elysium::Core::Template::Container;
+using namespace Elysium::Core::Template::Functional;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests::Core::Template::Container
+{
+	TEST_CLASS(VectorOfBoolTests)
+	{
+		TEST_METHOD(Constructors)
+		{
+
+		}
+	};
+}
