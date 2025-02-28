8a72234ba914752bb607569c05102d997e579d7e
waYne <cm.pillarbox@gmail.com>
2025-02-28 08:50:17 +0100

    further work on arenas

--- Libraries/01-Shared/Elysium.Core.Template/Memory/Scoped/Arena.hpp
+++ Libraries/01-Shared/Elysium.Core.Template/Memory/Scoped/Arena.hpp
@@ -41,8 +41,12 @@ Copyright (c) waYne (CAM). All rights reserved.
 	#include <processthreadsapi.h>
 	#endif
 #endif
+
 namespace Elysium::Core::Template::Memory::Scoped
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	class Arena
 	{
 	public:
@@ -58,25 +62,71 @@ namespace Elysium::Core::Template::Memory::Scoped
 
 		Arena& operator=(Arena&& Right) noexcept = delete;
 	public:
-		const Elysium::Core::Template::System::size GetTotalSize() const;
+		/// <summary>
+		/// Returns the number of pages currently allocated.
+		/// </summary>
+		/// <returns></returns>
+		const Elysium::Core::Template::System::size GetNumberOfPages() const;
+
+		/// <summary>
+		/// Returns the total capacity overarching all pages currently allocated.
+		/// </summary>
+		/// <returns></returns>
+		const Elysium::Core::Template::System::size GetCapacity() const;
+
+		/// <summary>
+		/// Returns total number of bytes used overarching all pages currently allocated.
+		/// </summary>
+		/// <returns></returns>
+		const Elysium::Core::Template::System::size GetSize() const;
 	public:
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="Size"></param>
+		/// <param name="AlignSize"></param>
+		/// <returns></returns>
 		void* Push(const Elysium::Core::Template::System::size Size, 
 			const Elysium::Core::Template::System::size AlignSize = alignof(Elysium::Core::Template::System::size)) noexcept;
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="NumberOfElements"></param>
+		/// <returns></returns>
 		template <Elysium::Core::Template::Concepts::Trivial T>
 		T* Push(const Elysium::Core::Template::System::size NumberOfElements = 1) noexcept;
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="Size"></param>
+		/// <param name="AlignSize"></param>
 		void Pop(const Elysium::Core::Template::System::size Size, 
 			const Elysium::Core::Template::System::size AlignSize = alignof(Elysium::Core::Template::System::size)) noexcept;
 		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="NumberOfElements"></param>
 		template <Elysium::Core::Template::Concepts::Trivial T>
 		void Pop(const Elysium::Core::Template::System::size NumberOfElements = 1) noexcept;
 		
+		/// <summary>
+		/// Clears the arena, keeping all pages and allocated memory.
+		/// </summary>
 		void Clear() noexcept;
 
-		//void Reset();
+		/// <summary>
+		/// Destroys all pages except the first one which will be cleared, keeping it's allocated memory.
+		/// </summary>
+		void Reset() noexcept;
 	private:
 		ArenaPage* CreatePage(const Elysium::Core::Template::System::size PageSize, ArenaPage* PreviousPage = nullptr) noexcept;
+
+		ArenaPage* DestroyPage(ArenaPage* Page) noexcept;
 	private:
 		const ArenaOptions _Options;
 		ArenaPage* _CurrentPage;
@@ -88,21 +138,42 @@ namespace Elysium::Core::Template::Memory::Scoped
 
 	inline Arena::~Arena() noexcept
 	{
-		Clear();
-
 		ArenaPage* CurrentPage = _CurrentPage;
-		ArenaPage* ToBeDeleted = nullptr;
 		while (CurrentPage != nullptr)
 		{
-			ToBeDeleted = CurrentPage;
-
-			CurrentPage = CurrentPage->_NextPage;
-
-			delete ToBeDeleted;
+			CurrentPage = DestroyPage(CurrentPage);
 		}
 	}
 
-	inline const Elysium::Core::Template::System::size Arena::GetTotalSize() const
+	inline const Elysium::Core::Template::System::size Arena::GetNumberOfPages() const
+	{
+		Elysium::Core::Template::System::size NumberOfPages = 0;
+
+		ArenaPage* CurrentPage = _CurrentPage;
+		while (CurrentPage != nullptr)
+		{
+			++NumberOfPages;
+			CurrentPage = CurrentPage->_NextPage;
+		}
+
+		return NumberOfPages;
+	}
+
+	inline const Elysium::Core::Template::System::size Arena::GetCapacity() const
+	{
+		Elysium::Core::Template::System::size TotalCapacity = 0;
+
+		ArenaPage* CurrentPage = _CurrentPage;
+		while (CurrentPage != nullptr)
+		{
+			TotalCapacity += CurrentPage->_Capacity;
+			CurrentPage = CurrentPage->_NextPage;
+		}
+
+		return TotalCapacity;
+	}
+
+	inline const Elysium::Core::Template::System::size Arena::GetSize() const
 	{
 		Elysium::Core::Template::System::size TotalSize = 0;
 
@@ -133,16 +204,27 @@ namespace Elysium::Core::Template::Memory::Scoped
 		Elysium::Core::Template::System::byte* CurrentAddress = &CurrentPage->_Data[CurrentPage->_CurrentOffset];
 
 		// use a bithack to calculate modulus "reinterpret_cast<Elysium::Core::Template::System::uint64_t>(CurrentAddress) % AlignSize"
-		const Elysium::Core::Template::System::uint64_t Padding =
-			(~reinterpret_cast<Elysium::Core::Template::System::uint64_t>(CurrentAddress) + 1) & (AlignSize - 1);
+		const Elysium::Core::Template::System::size Padding =
+			(~reinterpret_cast<Elysium::Core::Template::System::size>(CurrentAddress) + 1) & (AlignSize - 1);
+		const Elysium::Core::Template::System::size AdditionalSize = Padding + Size;
 
-		CurrentAddress += Padding;
-		if (CurrentAddress + Size > &CurrentPage->_Data[CurrentPage->_Capacity])
-		{	// @ToDo: resize strategy?
-			return nullptr;
+		if (CurrentAddress + AdditionalSize > &CurrentPage->_Data[CurrentPage->_Capacity])
+		{	
+			// I need to regard _CurrentPage as being "fully used", simply because "allocating" x numbers of elements on this page
+			// and the rest on the next page simply won't work!
+			// @ToDo: can think about splitting "memory-push" and "-usage" into seperate methods to make it possible to not "waste" memory.
+			// As it stands it's the user's responsibiliy to manage memory.
+			_CurrentPage->_CurrentOffset = _CurrentPage->_Capacity;
+
+			const Elysium::Core::Template::System::size PageSize = AdditionalSize > _Options._InitialPageSize ? 
+				AdditionalSize : _Options._InitialPageSize;
+			_CurrentPage = CreatePage(PageSize, _CurrentPage);
+
+			// @ToDo: add "void PushInternally(...)" which checks the depth of the recurions making sure it only happens once!
+			return Push(Size, AlignSize);
 		}
 
-		CurrentPage->_CurrentOffset += (Padding + Size);
+		CurrentPage->_CurrentOffset += AdditionalSize;
 
 		return CurrentAddress;
 	}
@@ -187,8 +269,7 @@ namespace Elysium::Core::Template::Memory::Scoped
 		const Elysium::Core::Template::System::uint64_t Padding =
 			(~reinterpret_cast<Elysium::Core::Template::System::uint64_t>(CurrentAddress) + 1) & (AlignSize - 1);
 
-		CurrentAddress -= Padding;
-		if (CurrentAddress - Size < CurrentPage->_Data)
+		if (CurrentAddress - Padding - Size < CurrentPage->_Data)
 		{	// @ToDo: this seems like an actual error on the user's part!
 			return;
 		}
@@ -210,34 +291,37 @@ namespace Elysium::Core::Template::Memory::Scoped
 		ArenaPage* CurrentPage = _CurrentPage;
 		while (CurrentPage != nullptr)
 		{
-			if (CurrentPage->_Data != nullptr)
-			{
-				if (_Options._UseVirtualPaging)
-				{
-#if defined ELYSIUM_CORE_OS_WINDOWS
-					BOOL Result = VirtualFreeEx(GetCurrentProcess(), CurrentPage->_Data, 0, MEM_RELEASE);
-					if (Result == FALSE)
-					{
-						// @ToDo: assert
-						throw 1;
-					}
-#else
-#error "unhandled operating system"
-#endif
-				}
-				else
-				{
-					delete[] CurrentPage->_Data;
-				}
-				CurrentPage->_Data = nullptr;
-			}
-
+			CurrentPage->_CurrentOffset = 0;
 			CurrentPage = CurrentPage->_NextPage;
+		}
+	}
+
+	inline void Arena::Reset() noexcept
+	{
+		// only clear the first page
+		ArenaPage* CurrentPage = _CurrentPage;
+		CurrentPage->_CurrentOffset = 0;
+
+		ArenaPage* PreviousPage = CurrentPage;
+		CurrentPage = CurrentPage->_NextPage;
+		PreviousPage->_NextPage = nullptr;
+
+		// destroy all other pages
+		while (CurrentPage != nullptr)
+		{
+			CurrentPage = DestroyPage(CurrentPage);
 		}
 	}
 
 	inline ArenaPage* Arena::CreatePage(const Elysium::Core::Template::System::size PageSize, ArenaPage* PreviousPage) noexcept
 	{
+		/*
+		// Since this method is only used internally, I do not need to check.
+		if (PageSize == 0)
+		{
+			return nullptr;
+		}
+		*/
 		void* Data = nullptr;
 
 		if (_Options._UseVirtualPaging)
@@ -256,7 +340,7 @@ namespace Elysium::Core::Template::Memory::Scoped
 		if (Data == nullptr)
 		{
 			// @ToDo: assert
-			throw 1;
+			bool bla = false;
 		}
 
 		if (_Options._ClearMemory)
@@ -271,6 +355,44 @@ namespace Elysium::Core::Template::Memory::Scoped
 		Page->_NextPage = PreviousPage;
 
 		return Page;
+	}
+
+	inline ArenaPage* Arena::DestroyPage(ArenaPage* Page) noexcept
+	{
+		/*
+		// Since this method is only used internally, I do not need to check.
+		if (Page == nullptr)
+		{
+			return;
+		}
+		*/
+		if (Page->_Data != nullptr)
+		{
+			if (_Options._UseVirtualPaging)
+			{
+#if defined ELYSIUM_CORE_OS_WINDOWS
+				BOOL Result = VirtualFreeEx(GetCurrentProcess(), Page->_Data, 0, MEM_RELEASE);
+				if (Result == FALSE)
+				{
+					// @ToDo: assert
+					bool bla = false;
+				}
+#else
+#error "unhandled operating system"
+#endif
+			}
+			else
+			{
+				delete[] Page->_Data;
+			}
+			Page->_Data = nullptr;
+		}
+
+		ArenaPage* NextPage = Page->_NextPage;
+
+		delete Page;
+
+		return NextPage;
 	}
 }
 #endif
--- UnitTests/01-Shared/UnitTests.Core.Template/ArenaTests.cpp
+++ UnitTests/01-Shared/UnitTests.Core.Template/ArenaTests.cpp
@@ -78,10 +78,13 @@ namespace UnitTests::Core::Template::Memory::Scoped
 			Elysium::Core::Template::Memory::Scoped::ArenaOptions Options = Elysium::Core::Template::Memory::Scoped::ArenaOptions(23, 1,
 				false, false);
 			Elysium::Core::Template::Memory::Scoped::Arena ArenaAllocator = Elysium::Core::Template::Memory::Scoped::Arena(Options);
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
 
 			// assign more bytes at once than defined page-size
 			void* AllocatedMemory0 = ArenaAllocator.Push(24);
 			Assert::IsNotNull(AllocatedMemory0);
+
+			Assert::Fail();
 		}
 
 		TEST_METHOD(ComplexTypeOnly)
@@ -90,6 +93,7 @@ namespace UnitTests::Core::Template::Memory::Scoped
 				sizeof(NonTrivialClass), 1,
 				false, false);
 			Elysium::Core::Template::Memory::Scoped::Arena ArenaAllocator = Elysium::Core::Template::Memory::Scoped::Arena(Options);
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
 
 			// push a single element
 			void* AllocatedMemory0 = ArenaAllocator.Push(sizeof(NonTrivialClass));
@@ -101,6 +105,7 @@ namespace UnitTests::Core::Template::Memory::Scoped
 
 
 			bool bla = false;
+			Assert::Fail();
 		}
 
 		TEST_METHOD(MixedTrivial)
@@ -108,6 +113,7 @@ namespace UnitTests::Core::Template::Memory::Scoped
 			Elysium::Core::Template::Memory::Scoped::ArenaOptions Options = Elysium::Core::Template::Memory::Scoped::ArenaOptions(80, 1,
 				false, false);
 			Elysium::Core::Template::Memory::Scoped::Arena ArenaAllocator = Elysium::Core::Template::Memory::Scoped::Arena(Options);
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
 
 			Assert::Fail();
 		}
@@ -119,33 +125,43 @@ namespace UnitTests::Core::Template::Memory::Scoped
 			Elysium::Core::Template::Memory::Scoped::ArenaOptions Options = Elysium::Core::Template::Memory::Scoped::ArenaOptions(
 				sizeof(Elysium::Core::Template::System::int32_t) * TotalNumberOfElements, 1, false, false);
 			Elysium::Core::Template::Memory::Scoped::Arena ArenaAllocator = Elysium::Core::Template::Memory::Scoped::Arena(Options);
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
 
 			// pop a single element (to ensure nothing happens)
 			ArenaAllocator.Pop<Elysium::Core::Template::System::int32_t>();
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
+
+			// reset the arena
+			ArenaAllocator.Reset();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 
 			// push no elements
 			Elysium::Core::Template::System::int32_t* AllocatedInstance0 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>(0);
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 			Assert::IsNull(AllocatedInstance0);
 
-			// push more elements than the arena can hold
-			Elysium::Core::Template::System::int32_t* AllocatedInstance1 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>(
-				TotalNumberOfElements + 1);
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
-			Assert::IsNull(AllocatedInstance1);
-
 			// push a single element
-			Elysium::Core::Template::System::int32_t* AllocatedInstance2 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>();
-			Assert::AreEqual(4_ui64, ArenaAllocator.GetTotalSize());
-			Assert::IsNotNull(AllocatedInstance2);
-			Assert::AreEqual(0, *AllocatedInstance2);
-			*AllocatedInstance2 = 2;
-			Assert::AreEqual(2, *AllocatedInstance2);
+			Elysium::Core::Template::System::int32_t* AllocatedInstance1 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(4_ui64, ArenaAllocator.GetSize());
+			Assert::IsNotNull(AllocatedInstance1);
+			Assert::AreEqual(0, *AllocatedInstance1);
+			*AllocatedInstance1 = 2;
+			Assert::AreEqual(2, *AllocatedInstance1);
 
 			// push an array of elements
 			int* AllocatedInstances0 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>(10);
-			Assert::AreEqual(44_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(44_ui64, ArenaAllocator.GetSize());
 			for (Elysium::Core::Template::System::uint8_t i = 0; i < 10; i++)
 			{
 				AllocatedInstances0[i] = i + 3;
@@ -154,19 +170,47 @@ namespace UnitTests::Core::Template::Memory::Scoped
 
 			// pop no elements
 			ArenaAllocator.Pop<Elysium::Core::Template::System::int32_t>(0);
-			Assert::AreEqual(44_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(44_ui64, ArenaAllocator.GetSize());
 
 			// pop more elements than the arena holds
 			ArenaAllocator.Pop<Elysium::Core::Template::System::int32_t>(TotalNumberOfElements - 1);
-			Assert::AreEqual(44_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(44_ui64, ArenaAllocator.GetSize());
 
 			// pop a single element
 			ArenaAllocator.Pop<Elysium::Core::Template::System::int32_t>();
-			Assert::AreEqual(40_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(40_ui64, ArenaAllocator.GetSize());
 
 			// pop a single element of twice the size
 			ArenaAllocator.Pop<Elysium::Core::Template::System::int64_t>();
-			Assert::AreEqual(32_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(32_ui64, ArenaAllocator.GetSize());
+
+			// push more elements than the arena can hold (forcing "resize")
+			Elysium::Core::Template::System::int32_t* AllocatedInstances1 = ArenaAllocator.Push<Elysium::Core::Template::System::int32_t>(
+				TotalNumberOfElements + 1);
+			Assert::AreEqual(2_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(164_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(164_ui64, ArenaAllocator.GetSize());
+			Assert::IsNotNull(AllocatedInstances1);
+
+			// clear the arena
+			ArenaAllocator.Clear();
+			Assert::AreEqual(2_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(164_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
+
+			// reset the arena again
+			ArenaAllocator.Reset();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(84_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 		}
 
 		TEST_METHOD(TrivialStructsOnly)
@@ -176,32 +220,43 @@ namespace UnitTests::Core::Template::Memory::Scoped
 			Elysium::Core::Template::Memory::Scoped::ArenaOptions Options = Elysium::Core::Template::Memory::Scoped::ArenaOptions(
 				sizeof(TrivialStruct) * TotalNumberOfElements, 1, false, false);
 			Elysium::Core::Template::Memory::Scoped::Arena ArenaAllocator = Elysium::Core::Template::Memory::Scoped::Arena(Options);
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
 
 			// pop a single element (to ensure nothing happens)
 			ArenaAllocator.Pop<TrivialStruct>();
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
+
+			// reset the arena
+			ArenaAllocator.Reset();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 
 			// push no elements
 			TrivialStruct* AllocatedInstance0 = ArenaAllocator.Push<TrivialStruct>(0);
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 			Assert::IsNull(AllocatedInstance0);
 
-			// push more elements than the arena can hold
-			TrivialStruct* AllocatedInstance1 = ArenaAllocator.Push<TrivialStruct>(TotalNumberOfElements + 1);
-			Assert::AreEqual(0_ui64, ArenaAllocator.GetTotalSize());
-			Assert::IsNull(AllocatedInstance1);
-
 			// push a single element
-			TrivialStruct* AllocatedInstance2 = ArenaAllocator.Push<TrivialStruct>();
-			Assert::AreEqual(8_ui64, ArenaAllocator.GetTotalSize());
-			Assert::IsNotNull(AllocatedInstance2);
-			Assert::AreEqual(0, AllocatedInstance2->_SomeInt);
-			AllocatedInstance2->_SomeInt = 2;
-			Assert::AreEqual(2, AllocatedInstance2->_SomeInt);
+			TrivialStruct* AllocatedInstance1 = ArenaAllocator.Push<TrivialStruct>();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(8_ui64, ArenaAllocator.GetSize());
+			Assert::IsNotNull(AllocatedInstance1);
+			Assert::AreEqual(0, AllocatedInstance1->_SomeInt);
+			AllocatedInstance1->_SomeInt = 2;
+			Assert::AreEqual(2, AllocatedInstance1->_SomeInt);
 
 			// push an array of elements
 			TrivialStruct* AllocatedInstances0 = ArenaAllocator.Push<TrivialStruct>(10);
-			Assert::AreEqual(88_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(88_ui64, ArenaAllocator.GetSize());
 			for (Elysium::Core::Template::System::uint8_t i = 0; i < 10; i++)
 			{
 				AllocatedInstances0[i]._SomeInt = i + 3;
@@ -210,19 +265,46 @@ namespace UnitTests::Core::Template::Memory::Scoped
 
 			// pop no elements
 			ArenaAllocator.Pop<TrivialStruct>(0);
-			Assert::AreEqual(88_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(88_ui64, ArenaAllocator.GetSize());
 
 			// pop more elements than the arena holds
 			ArenaAllocator.Pop<TrivialStruct>(TotalNumberOfElements - 1);
-			Assert::AreEqual(88_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(88_ui64, ArenaAllocator.GetSize());
 
 			// pop a single element
 			ArenaAllocator.Pop<TrivialStruct>();
-			Assert::AreEqual(80_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(80_ui64, ArenaAllocator.GetSize());
 
 			// pop a single element of twice the size
 			ArenaAllocator.Pop<TrivialStruct>(2);
-			Assert::AreEqual(64_ui64, ArenaAllocator.GetTotalSize());
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(160_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(64_ui64, ArenaAllocator.GetSize());
+
+			// push more elements than the arena can hold (forcing "resize")
+			TrivialStruct* AllocatedInstances1 = ArenaAllocator.Push<TrivialStruct>(TotalNumberOfElements + 1);
+			Assert::AreEqual(2_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(328_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(328_ui64, ArenaAllocator.GetSize());
+			Assert::IsNotNull(AllocatedInstances1);
+
+			// clear the arena
+			ArenaAllocator.Clear();
+			Assert::AreEqual(2_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(328_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
+
+			// reset the arena again
+			ArenaAllocator.Reset();
+			Assert::AreEqual(1_ui64, ArenaAllocator.GetNumberOfPages());
+			Assert::AreEqual(168_ui64, ArenaAllocator.GetCapacity());
+			Assert::AreEqual(0_ui64, ArenaAllocator.GetSize());
 		}
 	};
 }
