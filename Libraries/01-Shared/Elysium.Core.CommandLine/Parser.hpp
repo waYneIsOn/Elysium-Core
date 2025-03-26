3f2630d653f80a045f6f0b78c826a74f9576fb87
waYne <cm.pillarbox@gmail.com>
2025-03-26 23:29:35 +0100

    preparing commandline-projects

---
+++ Libraries/01-Shared/Elysium.Core.CommandLine/Parser.hpp
@@ -0,0 +1,21 @@
+/*
+===========================================================================
+
+Copyright (c) waYne (CAM). All rights reserved.
+
+===========================================================================
+*/
+#ifndef ELYSIUM_CORE_COMMANDLINE_PARSER
+#define ELYSIUM_CORE_COMMANDLINE_PARSER
+
+#ifdef _MSC_VER
+#pragma once
+#endif
+
+namespace Elysium::Core::CommandLine
+{
+	class Parser
+	{
+	};
+}
+#endif
