dd9ade2fcb0a18802c08593f3d93271c212c0d42
waYne <cm.pillarbox@gmail.com>
2025-07-26 09:22:55 +0200

    added quite a few cases of utf-8 validation

--- UnitTests/01-Shared/UnitTests.Core.Template/CharacterTraitsTests.cpp
+++ UnitTests/01-Shared/UnitTests.Core.Template/CharacterTraitsTests.cpp
@@ -256,7 +256,7 @@ namespace UnitTests::Core::Template::Text
 			constexpr const bool IsSimpleTextValid = CharacterTraits<char>::IsValid(SimpleText, STLength);
 			Assert::IsTrue(IsSimpleTextValid);
 
-			constexpr const bool IsSimpleText8Valid = CharacterTraits<char8_t>::IsValid(SimpleText8, ST8Length);
+			const bool IsSimpleText8Valid = CharacterTraits<char8_t>::IsValid(SimpleText8, ST8Length);
 			Assert::IsTrue(IsSimpleText8Valid);
 
 			constexpr const bool IsSimpleText16Valid = CharacterTraits<char16_t>::IsValid(SimpleText16, ST16Length);
@@ -412,6 +412,123 @@ namespace UnitTests::Core::Template::Text
 				Assert::IsFalse(IsInvalidWideTextValid);
 			}
 			*/
+		}
+
+		TEST_METHOD(IsInvalidUtf8)
+		{
+			// the following line should result in the byte sequence 0xC0 0xAF 0x00 but actually is 0xC0 0x80 0x00!
+			// const char8_t* OverlongEncoding = u8"\xC0\xAF";
+			// (might be the compiler interfering, meaning I cannot simply use that line though!!!)
+
+			// overlong encodings
+			constexpr const char8_t OverlongEncoding0[] = { 0xC0, 0xAF };	// 2 bytes
+			constexpr const char8_t OverlongEncoding1[] = { 0xE0, 0x80, 0xAF };	// 3 bytes
+			constexpr const char8_t OverlongEncoding2[] = { 0xF0, 0x80, 0x80, 0xAF };	// 4 bytes
+			constexpr const char8_t OverlongEncoding3[] = { 0xC0, 0xAF };	// 2 byte overlong for '/'
+			constexpr const char8_t OverlongEncoding4[] = { 0xE0, 0x80, 0xAF };	// 3 byte overlong for '/'
+			constexpr const char8_t OverlongEncoding5[] = { 0xC0, 0x80 };	// 2 byte overlong for NULL
+			constexpr const char8_t OverlongEncoding6[] = { 0xE0, 0x80, 0x80 };	// 3 byte overlong for NULL
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding0, CharacterTraits<char8_t>::GetLength(OverlongEncoding0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding1, CharacterTraits<char8_t>::GetLength(OverlongEncoding1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding2, CharacterTraits<char8_t>::GetLength(OverlongEncoding2)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding3, CharacterTraits<char8_t>::GetLength(OverlongEncoding3)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding4, CharacterTraits<char8_t>::GetLength(OverlongEncoding4)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding5, CharacterTraits<char8_t>::GetLength(OverlongEncoding5)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(OverlongEncoding6, CharacterTraits<char8_t>::GetLength(OverlongEncoding6)));
+
+			// incorrectly positioned lead bytes (for instance multiple in a row)
+			constexpr const char8_t MulipleLeadBytes0[] = { 0xC2, 0xC2 };	// lead byte implying a single trail bytes
+			constexpr const char8_t MulipleLeadBytes1[] = { 0xC0, 0xC0, 0xAF };	// lead byte implying two trail bytes
+			constexpr const char8_t MulipleLeadBytes2[] = { 0xE2, 0xE2, 0xA2 };	// lead byte implying three trail bytes
+			constexpr const char8_t MulipleLeadBytes3[] = { 0xF0, 0xF0, 0xAF, 0x80 };	// lead byte implying four trail bytes
+			constexpr const char8_t MulipleLeadBytes4[] = { 0xF0, 0x90, 0xE2, 0xA2 };	// lead byte in the middle of the sequence
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MulipleLeadBytes0, CharacterTraits<char8_t>::GetLength(MulipleLeadBytes0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MulipleLeadBytes1, CharacterTraits<char8_t>::GetLength(MulipleLeadBytes1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MulipleLeadBytes2, CharacterTraits<char8_t>::GetLength(MulipleLeadBytes2)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MulipleLeadBytes3, CharacterTraits<char8_t>::GetLength(MulipleLeadBytes3)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MulipleLeadBytes4, CharacterTraits<char8_t>::GetLength(MulipleLeadBytes4)));
+
+			// missing lead byte
+			constexpr const char8_t StartingTrailByt0e[] = { 0x80 };
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(StartingTrailByt0e, CharacterTraits<char8_t>::GetLength(StartingTrailByt0e)));
+
+			// missing trail byte
+			constexpr const char8_t MissingTrailByte0[] = { 0xC2 };	// needs one trail byte
+			constexpr const char8_t MissingTrailByte1[] = { 0xE1, 0x80 };	// needs two trail bytes
+			constexpr const char8_t MissingTrailByte2[] = { 0xF0, 0x90, 0x80 };	// needs three trail bytes
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MissingTrailByte0, CharacterTraits<char8_t>::GetLength(MissingTrailByte0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MissingTrailByte1, CharacterTraits<char8_t>::GetLength(MissingTrailByte1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(MissingTrailByte2, CharacterTraits<char8_t>::GetLength(MissingTrailByte2)));
+
+			// truncated sequence/premature ending ("special" form of missing trail byte)
+			constexpr const char8_t Truncated0[] = { 0xC2, 0xA2 };	// one trail byte
+			constexpr const char8_t Truncated1[] = { 0xE2, 0x82, 0xAC };	// two trail bytes
+			constexpr const char8_t Truncated2[] = { 0xF0, 0x9F, 0x92, 0x96 };	// three trail bytes
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Truncated0, CharacterTraits<char8_t>::GetLength(Truncated0) - sizeof(char8_t)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Truncated1, CharacterTraits<char8_t>::GetLength(Truncated1) - sizeof(char8_t)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Truncated2, CharacterTraits<char8_t>::GetLength(Truncated2) - sizeof(char8_t)));
+
+			// unexpected trail byte
+			constexpr const char8_t UnexpectedTrailByte0[] = { 0xE2, 0x82, 0xAC, 0xA0 };
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(UnexpectedTrailByte0, CharacterTraits<char8_t>::GetLength(UnexpectedTrailByte0)));
+
+			// utf-16
+			constexpr const char8_t Utf16Surrogates0[] = { 0xD8, 0x00 };	// high-surrogate
+			constexpr const char8_t Utf16Surrogates1[] = { 0xD8, 0x00, 0x00, 0x41 };	// two high-surrogates
+			constexpr const char8_t Utf16Surrogates2[] = { 0xD8, 0x00, 0x00, 0x41 };	// high-surrogate and non-low surrogate
+			constexpr const char8_t Utf16Surrogates3[] = { 0xDC, 0x00 };	// low-surrogate
+			constexpr const char8_t Utf16Surrogates4[] = { 0xDC, 0x00, 0xDC, 0x00 };	// two low-surrogates
+			constexpr const char8_t Utf16Surrogates5[] = { 0x41, 0xDC, 0x00 };	// valid utf8 and low-surrogate
+			constexpr const char8_t Utf16Surrogates6[] = { 0xDC, 0x01, 0xD8, 0x00 };	// low- and high surrogate
+			constexpr const char8_t Utf16Surrogates7[] = { 0x01, 0x61, 0xD8, 0x01, 0x01, 0x62 };	// high-surrogate in between valid utf8
+			constexpr const char8_t Utf16Surrogates8[] = { 0x01, 0x61, 0xDC, 0x01, 0x01, 0x62 };	// low-surrogate in between valid utf8
+			constexpr const char8_t Utf16Surrogates9[] = { 0xD8, 0x34, 0xDD, 0x1E };	// surrogate pair
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates0, CharacterTraits<char8_t>::GetLength(Utf16Surrogates0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates1, CharacterTraits<char8_t>::GetLength(Utf16Surrogates1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates2, CharacterTraits<char8_t>::GetLength(Utf16Surrogates2)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates3, CharacterTraits<char8_t>::GetLength(Utf16Surrogates3)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates4, CharacterTraits<char8_t>::GetLength(Utf16Surrogates4)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates5, CharacterTraits<char8_t>::GetLength(Utf16Surrogates5)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates6, CharacterTraits<char8_t>::GetLength(Utf16Surrogates6)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates7, CharacterTraits<char8_t>::GetLength(Utf16Surrogates7)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates8, CharacterTraits<char8_t>::GetLength(Utf16Surrogates8)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Utf16Surrogates9, CharacterTraits<char8_t>::GetLength(Utf16Surrogates9)));
+
+			// code-points outside unicode range (>U+10FFFF)
+			constexpr const char8_t UndefinedUnicodeCodePoint0[] = { 0xF4, 0x90, 0x80, 0x80 };	// U+110000
+			constexpr const char8_t UndefinedUnicodeCodePoint1[] = { 0xF8, 0x88, 0x80, 0x80, 0x80 };	// > 21 bits
+			constexpr const char8_t UndefinedUnicodeCodePoint2[] = { 0xFC, 0x80, 0x80, 0x80, 0x80, 0x80 };	// six byte sequence
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(UndefinedUnicodeCodePoint0, CharacterTraits<char8_t>::GetLength(UndefinedUnicodeCodePoint0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(UndefinedUnicodeCodePoint1, CharacterTraits<char8_t>::GetLength(UndefinedUnicodeCodePoint1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(UndefinedUnicodeCodePoint2, CharacterTraits<char8_t>::GetLength(UndefinedUnicodeCodePoint2)));
+
+			// non-character code-points
+			constexpr const char8_t NonCharacters0[] = { 0xEF, 0xB7, 0xAF };	// range of U+FDD0 – U+FDEF
+			constexpr const char8_t NonCharacters1[] = { 0xEF, 0xBF, 0xBE };	// ending with 0xFFFE
+			constexpr const char8_t NonCharacters2[] = { 0xEF, 0xBF, 0xBF };	// ending with 0xFFFF
+			constexpr const char8_t NonCharacters3[] = { 0xF0, 0x9F, 0xBF, 0xBE };	// supplementary plane ending with 0xFFFE
+			constexpr const char8_t NonCharacters4[] = { 0xF0, 0x9F, 0xBF, 0xBF };	// supplementary plane ending with 0xFFFF
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(NonCharacters0, CharacterTraits<char8_t>::GetLength(NonCharacters0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(NonCharacters1, CharacterTraits<char8_t>::GetLength(NonCharacters1)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(NonCharacters2, CharacterTraits<char8_t>::GetLength(NonCharacters2)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(NonCharacters3, CharacterTraits<char8_t>::GetLength(NonCharacters3)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(NonCharacters4, CharacterTraits<char8_t>::GetLength(NonCharacters4)));
+
+			// "old" utf-8 (pre-RFC-3629)
+			constexpr const char8_t Old0[] = { 0xF5 };	// utf-8 code-point represented by five bytes
+			constexpr const char8_t Old1[] = { 0xFF };	// utf-8 code-point represented by six bytes
+
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Old0, CharacterTraits<char8_t>::GetLength(Old0)));
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(Old1, CharacterTraits<char8_t>::GetLength(Old1)));
+
+			// BOM handling	- @ToDo: bom is only valid at the beginning, not in the middle. think about how I want to handle this!
+			constexpr const char8_t BOM[] = { 0xEF, 0xBB, 0xBF };
+			Assert::IsFalse(CharacterTraits<char8_t>::IsValid(BOM, CharacterTraits<char8_t>::GetLength(BOM)));
 		}
 	};
 }
