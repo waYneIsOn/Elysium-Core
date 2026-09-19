ba7024ccb4e98a4f241158567f898bcdcd73bfe9
waYne <cm.pillarbox@gmail.com>
2026-09-19 14:24:56 +0200

    this also works.......

--- UnitTests/01-Shared/UnitTests.Core.Template/TaskTests.cpp
+++ UnitTests/01-Shared/UnitTests.Core.Template/TaskTests.cpp
@@ -13,41 +13,69 @@ namespace UnitTests::Core::Template::Threading::Tasks
     public:
         TEST_METHOD(SinglularTaskTest)
         {
-            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformTaskDepthZero();
+            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformMostInternalTask();
             Task.Wait();
-            Assert::AreEqual(0_ui8, Task.GetResult());
+            Assert::AreEqual(1_ui8, Task.GetResult());
         }
 
-        TEST_METHOD(ChainedTaskTest)
+        TEST_METHOD(ChainedLoopedTaskTest)
         {
-            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task1 = PerformTaskDepthOne();
+            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task1 = PerformOuterLoopedTask(3_ui8);
             Task1.Wait();
-            Assert::AreEqual(1_ui8, Task1.GetResult());
-
-            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task2 = PerformTaskDepthTwo();
+            Assert::AreEqual(3_ui8, Task1.GetResult());
+            
+            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task2 = PerformOuterLoopedTask(24_ui8);
             Task2.Wait();
-            Assert::AreEqual(2_ui8, Task2.GetResult());
+            Assert::AreEqual(24_ui8, Task2.GetResult());
+        }
+
+        TEST_METHOD(ChainedRecursiveTaskTest)
+        {
+            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task1 = PerformOuterRecursiveTask(3_ui8);
+            Task1.Wait();
+            Assert::AreEqual(3_ui8, Task1.GetResult());
+
+            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task2 = PerformOuterRecursiveTask(24_ui8);
+            Task2.Wait();
+            Assert::AreEqual(24_ui8, Task2.GetResult());
         }
     private:
-        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformTaskDepthZero()
+        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformMostInternalTask()
         {
-            co_return 0;
+            co_return 1;
         }
 
-        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformTaskDepthOne()
+        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformOuterRecursiveTask(Elysium::Core::Template::System::uint8_t Iterations)
         {
-            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformTaskDepthZero();
-            co_await Task;
+            if (Iterations > 1)
+            {
+                Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformOuterRecursiveTask(--Iterations);
+                co_await Task;
 
-            co_return Task.GetResult() + 1;
+                co_return Task.GetResult() + 1;
+            }
+            else
+            {
+                Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformMostInternalTask();
+                co_await Task;
+
+                co_return Task.GetResult();
+            }
         }
 
-        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformTaskDepthTwo()
+        Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> PerformOuterLoopedTask(Elysium::Core::Template::System::uint8_t Iterations)
         {
-            Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformTaskDepthOne();
-            co_await Task;
+            Elysium::Core::Template::System::uint8_t Result{};
 
-            co_return Task.GetResult() + 1;
+            for (Elysium::Core::Template::System::uint8_t i = 0; i < Iterations; ++i)
+            {
+                Elysium::Core::Template::Threading::Tasks::Task<Elysium::Core::Template::System::uint8_t> Task = PerformMostInternalTask();
+                co_await Task;
+
+                Result += Task.GetResult();
+            }
+
+            co_return Result;
         }
     };
 }
